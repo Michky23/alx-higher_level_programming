de7f27c737899886f4d3c03a5ac1eb3d68e76788
Michky23 <michael23.micky@gmail.com>
2024-01-03 15:12:19 -0800

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for p in range(1, 101):
+        if p % 3 == 0 and p % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif p % 3 == 0:
+            print("Fizz ", end="")
+        elif p % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(p), end="")
