143d808dfb3aa3c523a8fd58ea06d1d8333020a8
Michky23 <michael23.micky@gmail.com>
2024-01-10 12:06:00 -0800

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+
+    """
+    update_dictionary:
+        function that replaces or adds key/value in a dictionary.
+
+    Args:
+        a_dictionary: the dictionary
+        key: the key
+        value: the value
+
+    Returns:
+    """
+    a_dictionary[key] = value
+    return (a_dictionary)
