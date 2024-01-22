86fad1285ef8154cbd688d30131aa5e687357660
Michky23 <michael23.micky@gmail.com>
2024-01-22 13:15:08 -0800

    Raise a message

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def raise_exception():
+
+    """
+    raise_exception:
+        function that raises a name exception with a message.
+
+    Arguments:
+        None
+
+    Returns:
+        None
+    """
+    raise TypeError("Incorrect Type")
